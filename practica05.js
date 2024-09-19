af052e7887e0083e8188f2e8bc32a9c15f620436
Joseline Vazquez <joselinevazquezfernandez21@gmail.com>
2024-09-19 14:43:24 -0600

    practica05

--- practica05.js
+++ practica05.js
@@ -1,11 +1,12 @@
 const app = document.getElementById("app")
     const url = "https:/picsum.photos/id/"
     const size = "/200/200"
+    //componente
     const picture = parametro => {
         const src=url+parametro+size
         return `
         <figure>
-        <img src="https://picsum.photos/id/${parametro}/200/200" alt="">
+        <img src="https://picsum.photos/id/${parametro}/200/200" alt="" id = "${parametro}" class ="">
         <figcaption>${parametro}</figcaption>
         </figure>
         `
@@ -15,14 +16,24 @@ const app = document.getElementById("app")
 
     app.innerHTML = picture(1) + picture(2) + picture(3)
 
-    const elemento = document.getElementById("app")
-    const elemento = document.getElementById("app")
-    const elemento = document.getElementById("app")
+    //const elemento1 = document.getElementById("1")
+    //const elemento2 = document.getElementById("2")
+    //const elemento3 = document.getElementById("3")
+    //elemento1.addEventListener("click", miFuncion)
+    //elemento2.addEventListener("click", miFuncion)
+    //elemento3.addEventListener("click", miFuncion)
+const lista = document.querySelectorAll("img")
+for (let i = 0; i < lista.length; i++) {
+    //cuando demos click se ejecute una funcion
+    lista[i].addEventListener("click", miFuncion)
+    
+}
 
-    elemento.addEventListener("click", miFuncion)
-    elementoaddEvenListener
+//funcion 
 
     function miFuncion(parametro){
-        console.log("ayyy" + parametro)
-        console.log(parametro)
+        console.log("ayyy")
+        console.log(parametro.target)
+        var imagen = parametro.target
+        imagen.classList.toggle("redonda")
 }
