346c94a6b92ce51354e871155a5ba0c70263e028
Joseline Vazquez <joselinevazquezfernandez21@gmail.com>
2024-10-03 15:07:56 -0600

    practica08

---
+++ xyz.js
@@ -0,0 +1,9 @@
+import * as miMoudlo from "./x.js"
+import {y} from"./y.js"
+import {z} from"./z.js"
+const app = document.getElementById("app")
+app.innerHTML = "App"
+console.log(miMoudlo.x("abc"))
+console.log(miModulo.xx("def"))
+console.log(y("ghi"))
+console.log(z("jkl"))
